76142de2ac04caa1da386f77f39dc566fda99ed2
Kevin La <kevinhla@yahoo.com>
2023-03-21 15:59:08 -0400

    Add func to play a round

--- script.js
+++ script.js
@@ -2,16 +2,40 @@ function getComputerChoice() {
     let roll = Math.floor(Math.random() * 3 + 1);
     if (roll === 1)
     {
-        return "Rock";
+        return "rock";
     }
     else if (roll === 2)
     {
-        return "Paper";
+        return "paper";
     }
     else
     {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection === computerSelection)
+    {
+        return 1;
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock")
+    {
+        return 2;
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection || playerSelection === "paper" && computerSelection === "scissors")
+    {
+        return 3;
+    }
+}
+
+
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
